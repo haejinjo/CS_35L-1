a18ceec52e7797e40803961c6b5bdc3946e6a875
ericchuu <eric.chuu71996@gmail.com>
2016-05-18 20:51:41 -0700

    hw7

--- lab7/create_test.c
+++ lab7/create_test.c
@@ -2,7 +2,7 @@
 
 int main(){
   int n = 0;
-  for (; n < 10; n++)
-    printf("mathematics");
+  for (; n < 1; n++)
+    printf("*~BO *{_CIA *hXE]D *LER #@_GZY #E\\OX #^BO #FKPS #NEM ");
   return 0;
 }
