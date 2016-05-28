874e4e2283a76e2c26e09b2ebbc2371f080128e5
Eric Chuu <chuu3ric@gmail.com>
2016-05-28 16:50:26 -0700

    submission

--- lab9/randmain.mk
+++ lab9/randmain.mk
@@ -10,9 +10,10 @@
 
 	# 3. after main is called, use dynamic linking via dlsym
 
+
+randmain: randmain.o randcpuid.o
+	$(CC) $(CFLAGS) randmain.o randcpuid.o -ldl -Wl,-rpath=$(PWD) -o randmain
 randlibhw.so: randlibhw.c
 	$(CC) $(CFLAGS) randlibhw.c -shared -fPIC -o randlibhw.so
 randlibsw.so: randlibsw.c
 	$(CC) $(CFLAGS) randlibsw.c -shared -fPIC -o randlibsw.so
-randmain: randmain.o randcpuid.o
-	$(CC) $(CFLAGS) randmain.o randcpuid.o -ldl -Wl,-rpath=$(PWD) -o randmain
